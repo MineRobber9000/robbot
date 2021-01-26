9b1e5144d28fd951169f817bb63ffa49d787b12c
khuxkm fbexl <khuxkm@ttm.sh>
2021-01-26 02:08:26 +0000

    Add advanced codeblock handling for eval function
    Code in question copied from rapptz/RoboDanny, which is licensed MPL-2.0.

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,5 +1,5 @@
 from discord.ext.commands import Cog, command, check, is_owner
-import os, traceback
+import os, traceback, textwrap
 
 class AdminCog(Cog, name="Admin"):
 	def __init__(self,bot):
@@ -38,6 +38,33 @@ class AdminCog(Cog, name="Admin"):
 	@command("list_modules",brief="Lists loaded modules.")
 	async def list_modules(self,ctx):
 		await ctx.send("```\nLoaded modules:\n"+"\n".join(self.bot.extensions)+"\n```")
+
+	def cleanup_code(self, content):
+		"""Automatically removes code blocks from the code."""
+		# remove ```py\n```
+		if content.startswith('```') and content.endswith('```'):
+			return '\n'.join(content.split('\n')[1:-1])
+
+		# remove `foo`
+		return content.strip('` \n')
+
+	@command("eval",brief="Evaluate Python code. Only Robert can do this.")
+	@is_owner()
+	async def robbot_eval(self,ctx,*,code):
+		try:
+			ns = dict()
+			ns["ctx"]=ctx
+			ns["bot"]=self.bot
+			ns["discord"]=__import__("discord")
+			__import__("discord.utils")
+			ns.update(globals())
+			code = "async def func():\n"+textwrap.indent(self.cleanup_code(code)," "*2)
+			exec(code,ns)
+			assert "func" in ns
+			ret = await ns["func"]()
+			await ctx.send("Result:\n```\n"+repr(ret)+"\n```\n")
+		except:
+			await ctx.send("Result:\n```\n"+traceback.format_exc()+"\n```\n")
 
 def setup(bot):
 	bot.add_cog(AdminCog(bot))
