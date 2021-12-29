f841aa29b38552a6bc86f08e9b57f0060efdda18
khuxkm fbexl <khuxkm@ttm.sh>
2021-12-29 03:18:39 +0000

    Fix up logging to flush stdout and stderr (as well as tagging bots)

--- cogs/log.py
+++ cogs/log.py
@@ -1,11 +1,16 @@
 from discord.ext.commands import Cog
+import sys
 #import aiofiles
 
 class Log(Cog,name="Logger"):
 	@Cog.listener()
 	async def on_message(self,message):
-		logline = f"[{message.channel.name}] {message.author.display_name}: {message.clean_content}"
+		userline = f"{message.author.display_name}"
+		if message.author.bot: userline+=" [BOT]"
+		logline = f"[{message.channel.name}] {userline}: {message.clean_content}"
 		print(logline)
+		sys.stdout.flush()
+		sys.stderr.flush()
 #		with aiofiles.open("log.txt","a") as f:
 #			await f.write(logline+"\n")
 #			await f.flush()
