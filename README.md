fc62abe593ac88e8c7cfaa514ef019e3ff9fd5d7
khuxkm fbexl <khuxkm@ttm.sh>
2021-12-29 03:17:07 +0000

    Remove the trello board (it's out of date

--- README.md
+++ README.md
@@ -5,7 +5,3 @@ My Discord bot for my server *con amigos*. Don't look directly at my code; you m
 ## Installation instructions
 
 Don't. Spare yourself the trouble, I'm pretty sure most of this spaghetti code only works because I pray several times a day that it won't break. If you really still want to give it a shot, put your bot token in `config.py` and run `bot.py`.
-
-## Trello board
-
-[I have a trello board for this.](https://trello.com/b/N22i5bkH/robbot)
--- cogs/rand.py
+++ cogs/rand.py
@@ -6,9 +6,9 @@ class RandCog(Cog, name="Random Stuff"):
 	@command(brief="Look at the source code of the bot")
 	async def source(self,ctx):
 		await ctx.send("https://github.com/MineRobber9000/robbot")
-	@command(brief="Look at the Trello board")
-	async def trello(self,ctx):
-		await ctx.send("https://trello.com/b/N22i5bkH/robbot")
+#	@command(brief="Look at the Trello board")
+#	async def trello(self,ctx):
+#		await ctx.send("https://trello.com/b/N22i5bkH/robbot")
 
 def setup(bot):
 	bot.add_cog(RandCog(bot))
